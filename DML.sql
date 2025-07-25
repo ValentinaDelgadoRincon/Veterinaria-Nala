f2c4368dc8f0f8cc8e45c762dcd6e1a3ab34a6fc
CamilaFlorez12 <07cmailaflorezsantos@gmail.com>
2025-07-25 09:11:39 -0500

    feat: :sparkles: add DML.sql
    add DML.sql
    
    By @CamilaFlorez

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO Duenios (nombre,telefono,direccion) VALUES
+("Emely Rojas","3221485255","Av.17 #17"),
+("Vanesa Diaz","3419564100","Calle 20 #20"),
+("Sara Martinez","3185410927","Cra.8 #8"),
+("David Perez","3183098322","Av.15 #15");
+
+INSERT INTO Mascotas (nombre,especie,raza,edad,sexo,vacunas) VALUES
+("Noha","perro","french poodle",5,"macho",FALSE),
+("Tomas","gato","siames",12,"macho",TRUE),
+("Odi","erizo","albino",4,"macho",FALSE),
+("Linda","perro","golden",4,"hembra",TRUE),
+("Casimiro","gato","ocicat",8,"macho",TRUE),
+("Nucita","gato","bombay",6,"hembra",TRUE),
+("Gus Gus","hamster","sirio",15,"macho",FALSE),
+("Romina","perro","doberman",2,"hembra",FALSE),
+("Shaina","tortuga","verde",9,"hembra",FALSE),
+("Tuttis","conejo","holandes",6,"macho",TRUE);
