26db1065410f2a93432253e885f984dc62b9b9b2
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-07-26 12:41:29 -0500

    feat: :sparkles: add consulta
    add consulta
    
    By @Valentinaa

--- DQL.sql
+++ DQL.sql
@@ -3,3 +3,5 @@ USE `Veterinaria Nala` ;
 SELECT CONCAT(D.nombre,' - ', M.especie) AS 'Dueños y mascotas' 
 FROM Duenios D 
 JOIN Mascotas M ON D.idDuenios = M.Duenios_idDuenios;
+
+DESCRIBE Duenios;
