51aa6fd2bf940835f02c281a92b307bfaf50525c
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-07-26 08:29:11 -0500

    fix: :bug: fixed some error
    fixed some error
    
    By@CamilaFlorez

--- DDL.sql
+++ DDL.sql
@@ -2,54 +2,46 @@
 -- -----------------------------------------------------
 -- Schema Veterinaria Nala
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Veterinaria Nala` ;
+CREATE DATABASE IF NOT EXISTS `Veterinaria Nala` ;
 USE `Veterinaria Nala` ;
 
 -- -----------------------------------------------------
 -- Table `Veterinaria Nala`.`Duenios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Veterinaria Nala`.`Duenios` (
-  `idDueños` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Duenios` (
+  `idDueños` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `nombre` VARCHAR(45) NOT NULL,
   `telefono` VARCHAR(45) NOT NULL,
-  `direccion` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idDueños`));
+  `direccion` VARCHAR(45) NOT NULL);
 
 
 -- -----------------------------------------------------
 -- Table `Veterinaria Nala`.`Servicio`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Veterinaria Nala`.`Servicio` (
-  `idServicio` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Servicio` (
+  `idServicio` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `nombre` VARCHAR(45) NOT NULL,
   `descripcion` VARCHAR(200) NOT NULL,
-  `precio` INT NOT NULL,
-  PRIMARY KEY (`idServicio`));
+  `precio` INT NOT NULL);
 
 
 
 -- -----------------------------------------------------
 -- Table `Veterinaria Nala`.`Visitas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Veterinaria Nala`.`Visitas` (
-  `idVisitas` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Visitas` (
+  `idVisitas` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `Servicio_idServicio` INT NOT NULL,
   `fecha` DATETIME NOT NULL,
-  PRIMARY KEY (`idVisitas`),
-  INDEX `fk_Visitas_Servicio1_idx` (`Servicio_idServicio` ASC) VISIBLE,
-  CONSTRAINT `fk_Visitas_Servicio1`
-    FOREIGN KEY (`Servicio_idServicio`)
-    REFERENCES `Veterinaria Nala`.`Servicio` (`idServicio`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    FOREIGN KEY (`Servicio_idServicio`)REFERENCES `Veterinaria Nala`.`Servicio` (`idServicio`));
 
 
 
 -- -----------------------------------------------------
 -- Table `Veterinaria Nala`.`Mascotas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Veterinaria Nala`.`Mascotas` (
-  `idMascotas` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Mascotas` (
+  `idMascotas` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `nombre` VARCHAR(45) NOT NULL,
   `especie` VARCHAR(45) NOT NULL,
   `raza` VARCHAR(45) NOT NULL,
@@ -58,48 +50,25 @@ CREATE TABLE IF NOT EXISTS `Veterinaria Nala`.`Mascotas` (
   `vacunas` TINYINT NOT NULL,
   `Dueños_idDUeños` INT NOT NULL,
   `Visitas_idVisitas` INT NOT NULL,
-  PRIMARY KEY (`idMascotas`, `Dueños_idDUeños`),
-  INDEX `fk_Mascotas_Dueños_idx` (`Dueños_idDUeños` ASC) VISIBLE,
-  INDEX `fk_Mascotas_Visitas1_idx` (`Visitas_idVisitas` ASC) VISIBLE,
-  CONSTRAINT `fk_Mascotas_Dueños`
-    FOREIGN KEY (`Dueños_idDUeños`)
-    REFERENCES `Veterinaria Nala`.`Duenios` (`idDueños`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Mascotas_Visitas1`
-    FOREIGN KEY (`Visitas_idVisitas`)
-    REFERENCES `Veterinaria Nala`.`Visitas` (`idVisitas`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    FOREIGN KEY (`Dueños_idDUeños`)REFERENCES `Veterinaria Nala`.`Duenios` (`idDueños`),
+    FOREIGN KEY (`Visitas_idVisitas`)REFERENCES `Veterinaria Nala`.`Visitas` (`idVisitas`));
 
 
 -- -----------------------------------------------------
 -- Table `Veterinaria Nala`.`Tratamientos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Veterinaria Nala`.`Tratamientos` (
-  `idTratamientos` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Tratamientos` (
+  `idTratamientos` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `nombre` VARCHAR(45) NOT NULL,
   `observaciones` VARCHAR(200) NOT NULL,
   `Visitas_idVisitas` INT NOT NULL,
-  PRIMARY KEY (`idTratamientos`),
-  INDEX `fk_Tratamientos_Visitas1_idx` (`Visitas_idVisitas` ASC) VISIBLE,
-  CONSTRAINT `fk_Tratamientos_Visitas1`
-    FOREIGN KEY (`Visitas_idVisitas`)
-    REFERENCES `Veterinaria Nala`.`Visitas` (`idVisitas`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    FOREIGN KEY (`Visitas_idVisitas`)REFERENCES `Veterinaria Nala`.`Visitas` (`idVisitas`));
 
 
 -- -----------------------------------------------------
 -- Table `Veterinaria Nala`.`Historial`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Veterinaria Nala`.`Historial` (
-  `idHistorial` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Historial` (
+  `idHistorial` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `Visitas_idVisitas` INT NOT NULL,
-  PRIMARY KEY (`idHistorial`),
-  INDEX `fk_Historial_Visitas1_idx` (`Visitas_idVisitas` ASC) VISIBLE,
-  CONSTRAINT `fk_Historial_Visitas1`
-    FOREIGN KEY (`Visitas_idVisitas`)
-    REFERENCES `Veterinaria Nala`.`Visitas` (`idVisitas`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    FOREIGN KEY (`Visitas_idVisitas`)REFERENCES `Veterinaria Nala`.`Visitas` (`idVisitas`));
--- DML.sql
+++ DML.sql
@@ -1,20 +1,22 @@
+USE `Veterinaria Nala`;
+
 INSERT INTO Duenios (nombre,telefono,direccion) VALUES
 ("Emely Rojas","3221485255","Av.17 #17"),
 ("Vanesa Diaz","3419564100","Calle 20 #20"),
 ("Sara Martinez","3185410927","Cra.8 #8"),
 ("David Perez","3183098322","Av.15 #15");
 
-INSERT INTO Mascotas (nombre,especie,raza,edad,sexo,vacunas) VALUES
-("Noha","perro","french poodle",5,"macho",FALSE),
-("Tomas","gato","siames",12,"macho",TRUE),
-("Odi","erizo","albino",4,"macho",FALSE),
-("Linda","perro","golden",4,"hembra",TRUE),
-("Casimiro","gato","ocicat",8,"macho",TRUE),
-("Nucita","gato","bombay",6,"hembra",TRUE),
-("Gus Gus","hamster","sirio",15,"macho",FALSE),
-("Romina","perro","doberman",2,"hembra",FALSE),
-("Shaina","tortuga","verde",9,"hembra",FALSE),
-("Tuttis","conejo","holandes",6,"macho",TRUE);
+INSERT INTO Mascotas (nombre,especie,raza,edad,sexo,vacunas,Dueños_idDUeños,Visitas_idVisitas) VALUES
+("Noha","perro","french poodle",5,"macho",FALSE,1,5),
+("Tomas","gato","siames",12,"macho",TRUE,4,2),
+("Odi","erizo","albino",4,"macho",FALSE,2,1),
+("Linda","perro","golden",4,"hembra",TRUE,1,3),
+("Casimiro","gato","ocicat",8,"macho",TRUE,3,4),
+("Nucita","gato","bombay",6,"hembra",TRUE,2,7),
+("Gus Gus","hamster","sirio",15,"macho",FALSE,4,6),
+("Romina","perro","doberman",2,"hembra",FALSE,1,9),
+("Shaina","tortuga","verde",9,"hembra",FALSE,3,10),
+("Tuttis","conejo","holandes",6,"macho",TRUE,2,8);
 
 INSERT INTO Servicio (nombre,descripcion,precio) VALUES
 ("Baño","Se realiza un baño completo con jabones y shampoos adecuados para cada mascota", 45000),
@@ -23,21 +25,21 @@ INSERT INTO Servicio (nombre,descripcion,precio) VALUES
 ("Desparasitacion", "Se da a la mascota los medicamentos correspondientes a su desparasitacion via oral", 65000),
 ("Vacunacion","Se realiza la vacunacion segun sea la indicada para la mascota, teniendo en cuenta su tamaño, peso y raza",15000);
 
-INSERT INTO Visitas (fecha) VALUES
-('30-07-2025 15:00'),
-('30-07-2025 15:30'),
-('02-08-2025 10:00'),
-('02-08-2025 16:00'),
-('05-08-2025 8:00'),
-('06-08-2025 13:00'),
-('10-08-2025 17:30'),
-('15-08-2025 14:00'),
-('15-08-2025 15:30'),
-('18-08-2025 07:00');
+INSERT INTO Visitas (Servicio_idServicio,fecha) VALUES
+(1,'2025-07-30 15:00'),
+(2,'2025-07-30 15:30'),
+(5,'2025-08-02 10:00'),
+(3,'2025-08-02 16:00'),
+(4,'2025-08-05 8:00'),
+(3,'2025-08-06 13:00'),
+(2,'2025-08-10 17:30'),
+(4,'2025-08-15 14:00'),
+(1,'2025-08-15 15:30'),
+(5,'2025-08-18 07:00');
 
-INSERT INTO Tratamientos(nombre,observaciones) VALUES
-("Cirugia","Se realiza una cirugia si es necesario para la mascota, para mejorar su salud"),
-("Odontologia","Se realiza limpieza bucal a cada una de las mascotas para evitar caries"),
-("Eutanasia","Se realizara este tratamiento a las mascotas que lo requieran por solicitud de su dueño"),
-("Spa","Se realizaran masajes, y diferentes proedimientos para relajar a tu mascota"),
-("Oncologia","Se realizara este tratamiento a mascotas que requieran este servicio");
+INSERT INTO Tratamientos(nombre,observaciones,Visitas_idVisitas) VALUES
+("Cirugia","Se realiza una cirugia si es necesario para la mascota, para mejorar su salud",1),
+("Odontologia","Se realiza limpieza bucal a cada una de las mascotas para evitar caries",5),
+("Eutanasia","Se realizara este tratamiento a las mascotas que lo requieran por solicitud de su dueño",2),
+("Spa","Se realizaran masajes, y diferentes proedimientos para relajar a tu mascota",3),
+("Oncologia","Se realizara este tratamiento a mascotas que requieran este servicio",4);
