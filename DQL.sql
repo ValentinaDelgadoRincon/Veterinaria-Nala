70cbb2d54399b0882560803039c2e2a8bb5164e9
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-07-26 12:19:05 -0500

    change

--- DQL.sql
+++ DQL.sql
@@ -1,4 +1,5 @@
 USE `Veterinaria Nala` ;
-SELECT CONCAT (D.nombre, ' - ', M.especie) AS 'Dueños y mascotas'
-FROM Duenios D
-JOIN Mascotas M ON D.idDueños = M.Dueños_idDUeños;
+
+SELECT CONCAT(D.nombre,' - ', M.especie) AS 'Dueños y mascotas' 
+FROM Duenios D 
+JOIN Mascotas M ON D.idDuenios = M.Duenios_idDuenios;
